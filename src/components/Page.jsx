f25f934bb7560343c63aa70664c30415aff3d92d
Shojib H. Ruhan <si.ruhan09@gmail.com>
2025-09-05 17:08:37 +0600

    initialize the project

---
+++ src/components/Page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className=" text-3xl text-cyan-700 font-semibold text-center">
+        Helllo
+      </h1>
+    </div>
+  );
+};
+
+export default Page;
